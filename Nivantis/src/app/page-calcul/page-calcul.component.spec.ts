c3329049c8a9fb974aeaefa3ad1f9e6700465a79
william turner <w.turner@mailoo.org>
2019-05-27 08:55:54 +0200

    test IHM pour calcul taux remise

--- Nivantis/src/app/page-calcul/page-calcul.component.spec.ts
+++ Nivantis/src/app/page-calcul/page-calcul.component.spec.ts
@@ -1,8 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PageCalculComponent } from './page-calcul.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 describe('PageCalculComponent', () => {
   let component: PageCalculComponent;
   let fixture: ComponentFixture<PageCalculComponent>;
@@ -11,6 +10,7 @@ describe('PageCalculComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ PageCalculComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [FormsModule, ReactiveFormsModule]
     })
     .compileComponents();
   }));
@@ -18,10 +18,17 @@ describe('PageCalculComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PageCalculComponent);
     component = fixture.componentInstance;
+    component.ngOnInit();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('quand insertion prix achat brut 30 et prix achat net 15 deverait retouner taux de remise 50 ', () => {
+    component.calculForm.controls.prixachatbrut.setValue(30);
+    component.calculForm.controls.prixachatnet.setValue(15);
+    expect(component.model.tauxRemise).toBe(50);
   });
 });
