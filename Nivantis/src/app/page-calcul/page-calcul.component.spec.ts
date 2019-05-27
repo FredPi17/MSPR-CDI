e562f7dd21090424ee7af61fe8926d1ccbdb79c8
Techlis <gorgetguilhem@gmail.com>
2019-05-27 09:00:06 +0200

    ajout test back taux remise

--- Nivantis/src/app/page-calcul/page-calcul.component.spec.ts
+++ Nivantis/src/app/page-calcul/page-calcul.component.spec.ts
@@ -1,7 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { PageCalculComponent } from './page-calcul.component';
+import {CalculData} from '../CalculData';
 
 describe('PageCalculComponent', () => {
   let component: PageCalculComponent;
@@ -9,8 +11,9 @@ describe('PageCalculComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule, ReactiveFormsModule ],
       declarations: [ PageCalculComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -23,5 +26,13 @@ describe('PageCalculComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Doit retourner un taux de remise de 50 quand le prix d\'achat net est de 15 et que le prix d\'achat brut est de 30', () => {
+    const calculData = new CalculData(0, 0, 0);
+    calculData.prixAchatBrut = 30;
+    calculData.prixAchatNet = 15;
+    const result = calculData.tauxRemise;
+    expect(result).toEqual(50);
   });
 });
