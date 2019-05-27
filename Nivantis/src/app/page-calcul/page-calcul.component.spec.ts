8d00ca9ae8a908ec760373d8a328e6ca76b0fc43
Techlis <gorgetguilhem@gmail.com>
2019-05-27 10:44:14 +0200

    ajout tests prix de vente net, coefficient multiplicateur, prix d'achat net'

--- Nivantis/src/app/page-calcul/page-calcul.component.spec.ts
+++ Nivantis/src/app/page-calcul/page-calcul.component.spec.ts
@@ -28,6 +28,7 @@ describe('PageCalculComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  // Taux de remise
   it('Doit retourner un taux de remise de 50 quand le prix d\'achat net est de 15 et que le prix d\'achat brut est de 30', () => {
     const calculData = new CalculData(0, 0, 0);
     calculData.prixAchatBrut = 30;
@@ -40,5 +41,36 @@ describe('PageCalculComponent', () => {
     component.calculForm.controls.prixachatbrut.setValue(30);
     component.calculForm.controls.prixachatnet.setValue(15);
     expect(component.model.tauxRemise).toBe(50);
+  });
+
+  // Prix d'achat net
+  it('Doit retourner un prix d\'achat net de 90 quand le prix d\'achat brut est de 100 et le taux de remise est 10%', () => {
+    const calculData = new CalculData(0, 0, 0);
+    calculData.prixAchatBrut = 100;
+    calculData.tauxRemise = 10;
+    const result = calculData.prixAchatNet;
+    expect(result).toBe(90);
+  });
+
+  // Coefficient multiplicateur
+  it('Doit retourner un coefficient multiplicateur de 1,25 quand le prix de vente net est de 100, ' +
+      'le prix d\'achat net est de 80 et le prix d\'achat brut est de 120', () => {
+    const calculData = new CalculData(0, 0, 0);
+    calculData.prixVenteNet = 100;
+    calculData.prixAchatBrut = 120;
+    calculData.prixAchatNet = 80;
+    const result = calculData.coefficient;
+    expect(result).toBe(1.25);
+  });
+
+
+  // Prix de vente net
+  it('Doit retourner un prix de vente net de 125 quand le prix d\'achat net est de 100' +
+      ' et le coefficient multiplicateur est de 1.25', () => {
+    const calculData = new CalculData(0, 0, 0);
+    calculData.prixAchatBrut = 100;
+    calculData.coefficient = 1.25;
+    const result = calculData.prixVenteNet;
+    expect(result).toBe(125);
   });
 });
