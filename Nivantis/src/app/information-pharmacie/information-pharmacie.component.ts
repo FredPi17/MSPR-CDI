2b61609fb624ecd8f6e9507af38979bde1c19a4c
Elsa <elsa.demaine@epsi.fr>
2019-06-03 11:51:08 +0200

    trier par getDistance

--- Nivantis/src/app/information-pharmacie/information-pharmacie.component.ts
+++ Nivantis/src/app/information-pharmacie/information-pharmacie.component.ts
@@ -32,70 +32,43 @@ export class InformationPharmacieComponent implements OnInit {
   information4: Informations;
   information5: Informations;
 
-ngOnInit() {
-  this.pharmacie1 = new Pharmacie(
-      1, 'St Bruno', '1 rue St Bruno',
-      2.34, 4.46,
-      25
-  );
-
-  this.pharmacie2 = new Pharmacie(
-      2,
-      'Des Arts',
-      '2 rue Des Arts',
-      3.345,
-      5.677,
-      2
-  );
-
-  this.pharmacie3 = new Pharmacie(
-      3,
-      'Victor Hugo',
-      '3 place Victor hugo',
-      7.34,
-      9.34534,
-      12
-  );
-
-  this.pharmacie4 = new Pharmacie(
-      4,
-      'Champolion',
-      '4 rue Champolion',
-      1.234,
-      8.45,
-      12
-  );
-  this.pharmacies.push(this.pharmacie1);
-  this.pharmacies.push(this.pharmacie2);
-  this.pharmacies.push(this.pharmacie3);
-  this.pharmacies.push(this.pharmacie4);
-  this.medicament1 = new Medicament(1, 'Aspirine', 1.50);
-  this.medicament2 = new Medicament(2, 'Doliprane 1000', 5.75);
-  this.medicament3 = new Medicament(3, 'Imeth', 2.00);
-  this.medicaments.push(this.medicament1);
-  this.medicaments.push(this.medicament2);
-  this.medicaments.push(this.medicament3);
-  this.information1 = new Informations(this.pharmacie1, this.medicament1, 10, 8, 'null');
-  this.information2 = new Informations(this.pharmacie1, this.medicament2, 30, 20, 'null');
-  this.information3 = new Informations(this.pharmacie2, this.medicament1, 5, 10, 'null');
-  this.information4 = new Informations(this.pharmacie3, this.medicament2, 15, 6, 'null');
-  this.information5 = new Informations(this.pharmacie4, this.medicament3, 13, 11, 'null');
-  this.informations.push(this.information1);
-  this.informations.push(this.information2);
-  this.informations.push(this.information3);
-  this.informations.push(this.information4);
-  this.informations.push(this.information5);
-}
-
-  onSave(){
-    console.log('todo save function');
+  ngOnInit() {
+    const lat = 8.45;
+    const lon = 1.234;
+    this.pharmacie1 = new Pharmacie( 1, 'St Bruno', '1 rue St Bruno', 2.34, 4.46, 25);
+    this.pharmacie2 = new Pharmacie(2, 'Des Arts', '2 rue Des Arts', 3.345, 5.677,2);
+    this.pharmacie3 = new Pharmacie(3, 'Victor Hugo', '3 place Victor hugo', 7.34, 9.34534, 12);
+    this.pharmacie4 = new Pharmacie(4, 'Champolion', '4 rue Champolion', 1.234, 8.45, 12);
+    this.pharmacies.push(this.pharmacie1);
+    this.pharmacies.push(this.pharmacie2);
+    this.pharmacies.push(this.pharmacie3);
+    this.pharmacies.push(this.pharmacie4);
+    this.pharmacies.sort((a, b) => {
+      return a.getDistance(Number(lat), Number(lon)) - b.getDistance(Number(lat), Number(lon));
+    });
+    this.medicament1 = new Medicament(1, 'Aspirine', 1.50);
+    this.medicament2 = new Medicament(2, 'Doliprane 1000', 5.75);
+    this.medicament3 = new Medicament(3, 'Imeth', 2.00);
+    this.medicaments.push(this.medicament1);
+    this.medicaments.push(this.medicament2);
+    this.medicaments.push(this.medicament3);
+    this.information1 = new Informations(this.pharmacie1, this.medicament1, 10, 8, 'null');
+    this.information2 = new Informations(this.pharmacie1, this.medicament2, 30, 20, 'null');
+    this.information3 = new Informations(this.pharmacie2, this.medicament1, 5, 10, 'null');
+    this.information4 = new Informations(this.pharmacie3, this.medicament2, 15, 6, 'null');
+    this.information5 = new Informations(this.pharmacie4, this.medicament3, 13, 11, 'null');
+    this.informations.push(this.information1);
+    this.informations.push(this.information2);
+    this.informations.push(this.information3);
+    this.informations.push(this.information4);
+    this.informations.push(this.information5);
   }
 
   getInformationPharmacie(id: number) {
     return this.informations.filter(x => x.pharmacie.id === id);
   }
 
-onSelect(id: number) {
+  onSelect(id: number) {
     this.selected = this.getInformationPharmacie(Number(id));
   }
 }
--- Nivantis/src/app/information-pharmacie/pharmacie.ts
+++ Nivantis/src/app/information-pharmacie/pharmacie.ts
@@ -1,3 +1,5 @@
+import {assertNumber} from "@angular/core/src/render3/assert";
+
 export class Pharmacie {
     public id: number;
     public nom: string;
@@ -13,5 +15,14 @@ export class Pharmacie {
         this.latitude = latitude;
         this.longitude = longitude;
         this.nbPersonnel = nbPersonnel;
+    }
+
+    getDistance(latitudeAcutelle : number, longitudeActuelle: number) : number {
+        var p = Math.PI / 180;
+        var c = Math.cos;
+        var a = 0.5 - c((this.latitude - latitudeAcutelle) * p) / 2 +
+            c(latitudeAcutelle * p) * c(this.latitude * p) *
+            (1 - c((this.longitude - longitudeActuelle) * p)) / 2;
+        return (12742 * Math.asin(Math.sqrt(a))) * 1000;
     }
 }
