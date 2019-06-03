5ec0cedbf8a555d29ea489fd348a1cf15e296863
Ezorken <paul.plouchard@gmail.com>
2019-06-03 09:48:12 +0200

    Correction routing + ajout lignes dans .gitignore

--- Nivantis/src/app/app-routing.module.ts
+++ Nivantis/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { InformationPharmacieComponent} from "./information-pharmacie/information-pharmacie.component";
+import {PageCalculComponent} from "./page-calcul/page-calcul.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'informationPharmacies', component: InformationPharmacieComponent},
+  { path: 'pageCalcul', component: PageCalculComponent}
 ];
 
 @NgModule({
