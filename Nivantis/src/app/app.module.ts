6287338ad2f60d8f50fddd81f56e57527adf5423
fred <fpinaud17@gmail.com>
2019-06-29 20:06:16 +0200

    Integration of the API on the project to show informations of Pharmacies

--- Nivantis/src/app/app.module.ts
+++ Nivantis/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -23,6 +24,7 @@ import { PageCalculComponent} from './page-calcul/page-calcul.component';
     PageCalculComponent],
   entryComponents: [],
   imports: [
+    HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
--- Nivantis/src/app/information-pharmacie/informations.ts
+++ Nivantis/src/app/information-pharmacie/informations.ts
@@ -1,18 +1,15 @@
-import {Pharmacie} from './pharmacie';
-import {Medicament} from './medicament';
-
 export class Informations {
-    public pharmacie: Pharmacie;
-    public medicament: Medicament;
     public besoin: number;
     public ventes: number;
     public derniereFormation: string;
+    public nom: string;
+    public prix: number;
 
-    constructor(pharmacie, medicament, besoin, ventes, derniereFormation) {
-        this.pharmacie = pharmacie;
-        this.medicament = medicament;
+    constructor(besoin, ventes, derniereFormation, nom, prix) {
         this.besoin = besoin;
         this.ventes = ventes;
         this.derniereFormation = derniereFormation;
+        this.nom = nom;
+        this.prix = prix;
     }
 }
