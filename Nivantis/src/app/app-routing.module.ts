448c18bb0eca406107879a323bdee95ee6035696
unknown <w.turner@mailoo.org>
2019-07-01 20:53:53 +0200

    ajout login composent

--- Nivantis/src/app/app-routing.module.ts
+++ Nivantis/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {InformationPharmacieComponent} from './information-pharmacie/information-pharmacie.component';
 import {PageCalculComponent} from './page-calcul/page-calcul.component';
 import {AdminFormulaireComponent} from './admin-formulaire/admin-formulaire.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'informationPharmacies', component: InformationPharmacieComponent},
   {path: 'pageCalcul', component: PageCalculComponent},
-  {path: 'adminFormulaire', component: AdminFormulaireComponent}
+  {path: 'adminFormulaire', component: AdminFormulaireComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
